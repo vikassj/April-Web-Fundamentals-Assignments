6bac5652b9dfcad57cbefb8a05c4cd0a683ac4da
vikassj <67403023+vikassj@users.noreply.github.com>
2022-05-29 17:52:24 +0530

    Test 29th may

---
+++ May/week5/odd_even.js
@@ -0,0 +1,6 @@
+let num=5;
+if(num%2==0){
+    console.log('given number is even');
+}else{
+    console.log('given number is odd');
+}
---
+++ May/week5/square_rectangle.js
@@ -0,0 +1,7 @@
+let l=10;
+let b=20;
+if(l==b){
+  console.log('Its square');
+}else{
+  console.log('Its rectangle');
+}
